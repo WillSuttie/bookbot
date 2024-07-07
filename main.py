3f4c62f0c977c16556cdd07c20dd9fe1f90b3121
WillSutt <williamsuttie87@gmail.com>
2024-07-07 21:19:10 +0100

    report function

--- main.py
+++ main.py
@@ -3,8 +3,34 @@ def main():
     input_file = "books/frankenstein.txt"
     file_contents = get_book_text(input_file)
     word_list = word_counter(file_contents)
-    print (len(word_list))
+    character_dictionary = character_counter(file_contents)
+    character_reporter(character_dictionary, word_list, input_file)
     
+
+
+
+def character_reporter(character_dictionary, word_list, input_file ):
+    
+    ordered_dictionary = {}
+
+    while character_dictionary:
+        highest = (None, float('-inf'))
+        for char, count in character_dictionary.items():
+            if count > highest[1]:
+                highest = (char, count)
+        
+        
+        ordered_dictionary[highest[0]] = highest[1]
+        
+        
+        del character_dictionary[highest[0]]
+
+    print(f"--- Begin report of {input_file} ---")
+    print(f"{len(word_list)} words found in the document")
+    print(" ")
+    for char in ordered_dictionary:
+        print(f"{char} appeared {ordered_dictionary[char]} times.")
+    print("--- End Report ---")
 
 
 def get_book_text(input_file):
@@ -16,15 +42,16 @@ def word_counter(input_string):
     words = input_string.split()
     return words
 
-def charcter_counter(input_string):
+def character_counter(input_string):
     character_count = {}
     
     for char in input_string:
         char = char.lower()
         
-        if char in character_count:
-            character_count[char] += 1
-        else: character_count[char] = 1
+        if char.isalpha():
+            if char in character_count:
+                character_count[char] += 1
+            else: character_count[char] = 1
     
     return character_count
 
